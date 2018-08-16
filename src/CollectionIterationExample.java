f3a5108ba47584fbac70583140fdb1317821f653
gagan86nagpal <gagannagpal68@gmail.com>
2018-08-16 15:53:11 +0530

    Added CollectionIterationExample

---
+++ src/CollectionIterationExample.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionIterationExample {
+
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Gagan","Nagpal",21),
+                new Person("Shubham","Rustagi",21),
+                new Person("Akash","Verma",21),
+                new Person("Paritosh","Verma",22),
+                new Person("Sagar","Gupta",22),
+                new Person("Chirag","Chopra",23),
+                new Person("Abhishek","Cing",21)
+        );
+        System.out.println("Iterating the loop java 8 way");
+
+        people.forEach((p)-> System.out.println(p.getFirstName()));
+
+    }
+}

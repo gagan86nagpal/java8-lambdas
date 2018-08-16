53e55ef16d1c16a6e7b42c383230eda3ab73723d
gagan86nagpal <gagannagpal68@gmail.com>
2018-08-16 13:22:24 +0530

    Added file ThisReferenceExample.java

--- src/ThisReferenceExample.java
+++ src/ThisReferenceExample.java
@@ -6,5 +6,20 @@ public class ThisReferenceExample {
 
     public static void main(String[] args) {
 
+        ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
+
+
+        thisReferenceExample.doProcess(10, i-> {
+            System.out.println("i: " + i);
+            //System.out.println(this);
+            //cannot use this from a static context
+            // Value of this in a lambda expression is same as outside lambda
+        });
+        thisReferenceExample.doProcess(10, new Process() {
+            public void doProcess(int i) {
+                System.out.println("i: "+ i);
+                System.out.println(this);
+            }
+        });
     }
 }

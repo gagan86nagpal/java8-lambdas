841f792612f72267b843be42ae76cbaaf8df2c3a
Gagandeep Nagpal <gagannagpal68@gmail.com>
2018-08-16 12:37:51 +0530

    Wrapper for try catch and pass through functionsd

--- src/ExceptionHandlingExample.java
+++ src/ExceptionHandlingExample.java
@@ -4,9 +4,20 @@ public class ExceptionHandlingExample {
 
 
     public static void main(String[] args) {
-        int [] someArray ={1,2,3,4,5};
-        int key = 2;
-        process(someArray,key,(i,k) -> System.out.print(i/k+ " "));
+        int[] someArray = {1, 2, 3, 4, 5};
+        int key = 0;
+
+        process(someArray,key, wrapperlambda( (i,k) -> System.out.println(i/  k) ));
+    }
+    private static BiConsumer<Integer,Integer> wrapperlambda(BiConsumer<Integer,Integer> biConsumer) {
+        return (i,k) ->{
+            try {
+                biConsumer.accept(i,k);
+            }
+            catch (ArithmeticException ae) {
+                System.out.println("Arithmetic Exception occured");
+            }
+        };
     }
 
     public static void process(int[] someArray, int key, BiConsumer<Integer,Integer> biConsumer) {

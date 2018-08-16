e2152ed191696c040796e4d93b00bec7b469ae9d
gagan86nagpal <gagannagpal68@gmail.com>
2018-08-16 13:02:02 +0530

    Added idea files

---
+++ src/ClosuresExample.java
@@ -0,0 +1,15 @@
+public class ClosuresExample {
+
+    public static void main(String[] args) {
+        int b = 10;
+        doProcess(12,x-> System.out.println(x+b));
+    }
+
+    public static void doProcess(int a,Process p) {
+        p.doProcess(a);
+    }
+}
+
+interface Process {
+    void doProcess(int i);
+}
